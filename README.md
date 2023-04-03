0523a755805875d4d2f71f8f8035468319d0a250
sajeewa garusinghe <sajeeva616@gmail.com>
2023-04-03 20:36:55 +0530

    clear some git error

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # chat-app
 full chat app with login functionality
+
+
+https://api.multiavatar.com/B552.svg
+
+react-toastify
+buffer
---
+++ client/src/pages/SetAvatar.jsx
@@ -0,0 +1,14 @@
+import React, { useState,useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+ 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
+import { registerRoute } from '../utils/apiRoutes';
+
+const SetAvatar = () => {
+  return <div>A</div>;
+};
+
+export default SetAvatar;
